bf0c051423d0594688f4aa08045e6acedf3c3069
mhmdeve <mhmdeveloperofficial@gmail.com>
2021-09-26 09:01:50 +0200

    apollo: Removed MusicFX

--- device.mk
+++ device.mk
@@ -14,10 +14,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
-#MusicFX
-PRODUCT_PACKAGES += \
-MusicFX
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
