f6716e0b6ee0aa39918773d9ffeb117f615e3f39
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-10-07 10:17:26 +0200

    apollo: Weeb-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollo.mk
+    $(LOCAL_DIR)/weeb_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollo-user \
-    lineage_apollo-userdebug \
-    lineage_apollo-eng
+    weeb_apollo-user \
+    weeb_apollo-userdebug \
+    weeb_apollo-eng
--- weeb_apollo.mk
+++ weeb_apollo.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Weeb stuff.
+$(call inherit-product, vendor/weeb/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_apollo
+PRODUCT_NAME := weeb_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
